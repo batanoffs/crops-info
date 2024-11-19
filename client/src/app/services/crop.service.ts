47cc6ec346d08a59600697d2e061d132d783669d
batanoffs <batanoff.s@protonmail.com>
2024-11-19 17:14:38 +0200

    update api

--- client/src/app/services/crop.service.ts
+++ client/src/app/services/crop.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http'
 
 @Injectable()
 export class CropService {
-	private apiUrl = 'http://localhost:3000/api/crops'
+	private apiUrl = 'http://localhost:5000/api/vegetables'
 
 	constructor(private http: HttpClient) {}
 
