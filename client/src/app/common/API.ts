f3e4713383a3469f0ea13eb542ff421ae1be6011
batanoffs <batanoff.s@protonmail.com>
2024-11-29 16:54:40 +0200

    add api endpoints constants

---
+++ client/src/app/common/API.ts
@@ -0,0 +1,12 @@
+const baseURL = 'http://localhost:5000/api/'
+
+const endpoints = {
+	crops: baseURL + 'crops/',
+	auth: baseURL + 'auth/',
+}
+
+export const API = {
+	CATALOG: endpoints.crops,
+	LOGIN: endpoints.auth + 'login',
+	REGISTER: endpoints.auth + 'register',
+}
