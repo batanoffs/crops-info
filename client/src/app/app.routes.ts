5b2c29dba380ccb7e2aa84ba8c429cee039b0e71
batanoffs <batanoff.s@protonmail.com>
2024-12-03 16:46:17 +0200

    add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'login', component: LoginComponent },
+    // { path: 'register', component: RegisterComponent },
+    // { path: 'home', component: HomeComponent },
+    // { path: 'about', component: AboutComponent },
+    // { path: 'crops', component: CropsComponent },
+    // { path: 'favorites', component: FavoritesComponent },
+    // { path: 'crop/:id', component: CropDetailComponent },
+];
