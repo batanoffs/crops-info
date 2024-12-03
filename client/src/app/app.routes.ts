6e17d19513cfff065709f2b04bae7b6f72be5f75
batanoffs <batanoff.s@protonmail.com>
2024-12-03 17:20:24 +0200

    add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: AppComponent },
-    { path: 'login', component: LoginComponent },
-    // { path: 'register', component: RegisterComponent },
-    // { path: 'home', component: HomeComponent },
-    // { path: 'about', component: AboutComponent },
-    // { path: 'crops', component: CropsComponent },
-    // { path: 'favorites', component: FavoritesComponent },
-    // { path: 'crop/:id', component: CropDetailComponent },
+	{ path: '', component: AppComponent },
+	{ path: 'login', component: LoginComponent },
+	// { path: 'register', component: RegisterComponent },
+	{ path: 'home', component: HomeComponent },
+	// { path: 'about', component: AboutComponent },
+	{ path: 'crops', component: CatalogComponent },
+	// { path: 'favorites', component: FavoritesComponent },
+	// { path: 'crop/:id', component: CropDetailComponent },
 ];
