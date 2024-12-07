b2d68937c7e960fcf360cd69aa8a3228171048cc
batanoffs <batanoff.s@protonmail.com>
2024-12-07 14:21:40 +0200

    add test data

---
+++ client/src/app/pages/catalog/data.ts
@@ -0,0 +1,38 @@
+const testData = [
+    {
+    id: 1,
+    name: 'Tomato',
+    attributes: {
+        color: 'red',
+        size: 'small',
+        growthTime: '70 days',
+        },
+    },
+    {
+    id: 2,
+    name: 'Cucumber',
+    attributes: {
+        color: 'green',
+        size: 'long',
+        growthTime: '50 days',
+        },
+    },
+    {
+    id: 3,
+    name: 'Carrot',
+    attributes: {
+        color: 'orange',
+        size: 'small',
+        growthTime: '60 days',
+        },
+    },
+    {
+    id: 4,
+    name: 'Pepper',
+    attributes: {
+        color: 'green',
+        size: 'small',
+        growthTime: '70 days',
+        },
+    },
+]
