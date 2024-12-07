5cb5052a83c4b63f43baccc58f8046b14640061d
batanoffs <batanoff.s@protonmail.com>
2024-12-07 14:19:42 +0200

    update component with service and display array of items

--- client/src/app/pages/catalog/catalog.component.ts
+++ client/src/app/pages/catalog/catalog.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CropList } from './crop-list/crop-list.component';
+import { Crop } from './crop';
+import { CropService } from '../../services/crop.service';
 
 @Component({
-  selector: 'app-catalog',
-  standalone: true,
-  imports: [],
-  templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.scss'
+	selector: 'crops-catalog',
+	standalone: true,
+	imports: [CropList],
+	templateUrl: './catalog.component.html',
+	styleUrl: './catalog.component.scss',
+  providers: [CropService],
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+	crops: Crop[] = [];
 
+	constructor(private cropService: CropService) {}
+
+	ngOnInit(): void {
+		this.cropService.getCrops().subscribe((response: any) => {
+			this.crops = response;
+		});
+	}
 }
