6f2ce28db520929211068b0809097628c818b98d
batanoffs <batanoff.s@protonmail.com>
2024-12-07 14:17:52 +0200

    updated routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,16 +1,14 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-	{ path: '', component: AppComponent },
-	{ path: 'login', component: LoginComponent },
-	// { path: 'register', component: RegisterComponent },
 	{ path: 'home', component: HomeComponent },
-	// { path: 'about', component: AboutComponent },
+	{ path: 'login', component: LoginComponent },
 	{ path: 'crops', component: CatalogComponent },
+	// { path: 'register', component: RegisterComponent },
+	// { path: 'about', component: AboutComponent },
 	// { path: 'favorites', component: FavoritesComponent },
 	// { path: 'crop/:id', component: CropDetailComponent },
 ];
