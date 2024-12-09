1623514c98b5aef64a738df07f52c7998719787e
batanoffs <batanoff.s@protonmail.com>
2024-12-09 16:57:37 +0200

    removed

--- server/src/interfaces/Fruits.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document, Types } from 'mongoose'
-
-export interface IFruitsSchema extends Document {
-	name: string
-	description: string
-	pesticides: Types.ObjectId[]
-	picture: string
-	createdAt: Date
-}
