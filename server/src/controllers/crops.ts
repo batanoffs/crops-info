4ab80bdf318a57cf8d41b6f0d51018a260027157
batanoffs <batanoff.s@protonmail.com>
2024-12-09 16:59:06 +0200

    update controllers post get crops

--- server/src/controllers/crops.ts
+++ server/src/controllers/crops.ts
@@ -1,28 +1,110 @@
 import { NextFunction, Request, Response } from 'express'
-import Crops from '../models/Crops'
-/**
- * Adds a new category item to the database.
- *
- * @param {Request} req - Express request object.
- * @param {Response} res - Express response object.
- */
-export const addVeggie = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+import Crop from '../models/Crop'
+
+export const addCrop = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	if (!req.body) {
+		res.status(400).json({ message: 'No body provided' })
+		return
+	}
+
 	const {
-		body: { name },
-	} = req
+		name,
+		picture,
+		description,
+		attributes,
+		nutrition,
+		pests,
+		diseases,
+		companionPlants,
+		combativePlants,
+	} = req.body
+
+	if (!name || !picture || !description || !attributes) {
+		res.status(400).json({ message: 'Missing required fields' })
+		return
+	}
+
+	const {
+		spacing,
+		plantingDepth,
+		sun,
+		water,
+		frost,
+		soil,
+		sproutToMature,
+		germination,
+		sowingTime,
+		color,
+		size,
+		growthTime,
+	} = attributes as {
+		spacing: string
+		plantingDepth: string
+		sun: string
+		water: string
+		frost: string
+		soil: string
+		sproutToMature: string
+		germination: string
+		sowingTime: string
+		color: string
+		size: string
+		growthTime: string
+	}
+
+	if (
+		!spacing ||
+		!plantingDepth ||
+		!sun ||
+		!water ||
+		!frost ||
+		!soil ||
+		!sproutToMature ||
+		!germination ||
+		!sowingTime ||
+		!color ||
+		!size ||
+		!growthTime
+	) {
+		res.status(400).json({ message: 'Missing required fields in attributes' })
+		return
+	}
 
 	try {
+		const crop = await Crop.create({
+			name,
+			picture,
+			description,
+			attributes,
+			nutrition,
+			pests,
+			diseases,
+			companionPlants,
+			combativePlants,
+		})
+		res.json(crop)
 	} catch (error) {
 		res.status(500).json({ message: 'Error adding category item', error })
 	}
 }
 
-export const getAll = async (req: Request, res: Response) => {
+export const getAll = async (req: Request, res: Response): Promise<void> => {
 	try {
 		// Fetch all Crops
-		const crops = await Crops.find().lean().exec()
+		const crops = await Crop.find().lean().exec()
+		if (!crops || crops.length === 0) {
+			res.status(404).json({ message: 'No crops found' })
+			return
+		}
 		res.json(crops)
-	} catch (error) {
-		res.status(500).json({ message: 'Error fetching categories', error })
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			res.status(500).json({ message: 'Error fetching crops', error: error.message })
+		} else {
+			res.status(500).json({
+				message: 'Unknown error fetching crops',
+				error: 'Unknown error',
+			})
+		}
 	}
 }
