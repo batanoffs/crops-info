252c349f61c50931118df9fa6adfa8eea8070a30
batanoffs <batanoff.s@protonmail.com>
2024-12-09 16:59:59 +0200

    update api url

--- client/src/app/components/catalog/crop.service.ts
+++ client/src/app/components/catalog/crop.service.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import { API } from '../../common/serverApi'
-import { Observable } from 'rxjs'
+import { Injectable } from '@angular/core';
+import { API } from '../../common/serverApi';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Crop } from './crop.interface';
 
 @Injectable()
 export class CropService {
+	URL = 'http://localhost:5000/api/crops';
+
+	posts: Crop[] = [];
 	constructor(private http: HttpClient) {}
 
+	getPosts() {
+		return this.http.get(this.URL);
+	}
+
 	getCrops(): Observable<any> {
-		return this.http.get(API.CATALOG)
+		return this.http.get(API.CATALOG);
 	}
 
 	saveCrop(crop: any) {
-		return this.http.post(API.CATALOG, crop)
+		return this.http.post(API.CATALOG, crop);
 	}
 }
