02c11e9d0d647ca740c4778d9e3e967dab607048
batanoffs <batanoff.s@protonmail.com>
2024-12-10 17:25:36 +0200

    update interface

--- client/src/app/components/catalog/crop.interface.ts
+++ client/src/app/components/catalog/crop.interface.ts
@@ -1,9 +1,43 @@
+type Attributes = {
+	spacing: string;
+	plantingDepth: string;
+	sun: 'Full Sun' | 'Partial Sun' | 'Shade';
+	water: string;
+	frost: 'Not tolerant' | 'Tolerant' | 'Very tolerant';
+	soil: 'Acidic' | 'Neutral' | 'Alkaline';
+	sproutToMature: string;
+	germination: string;
+	sowingTime: string;
+	color: string;
+	size: string;
+	growthTime: string;
+};
+
+type Nutrition = {
+	vitamins: string[];
+	nutrients: string[];
+};
+
+type CompanionPlants = {
+	plant: string;
+	reasons: string[];
+};
+
+type CombatPlants = {
+	plant: string;
+	reasons: string[];
+};
+
 export interface Crop {
-	id: number;
+	_id: string;
 	name: string;
-	attributes: {
-		color: string;
-		size: string;
-		growthTime: string;
-	};
+	picture: string;
+	description: string;
+	attributes: Attributes;
+	nutrition: Nutrition;
+	pests: string[];
+	diseases: string[];
+	companionPlants: CompanionPlants[];
+	combativePlants: CombatPlants[];
+	createdAt: Date;
 }
