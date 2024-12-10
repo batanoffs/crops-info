1ac14af5e2db3093d837e3745b7e17d2164645c7
batanoffs <batanoff.s@protonmail.com>
2024-12-10 17:26:42 +0200

    update html and component

--- client/src/app/components/catalog/crop-list/crop-list.component.ts
+++ client/src/app/components/catalog/crop-list/crop-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Crop } from '../crop.interface';
+import { RouterLink } from '@angular/router';
+// private router: Router
 
 @Component({
 	selector: 'crop-list',
 	standalone: true,
-	imports: [],
+	imports: [RouterLink],
 	templateUrl: './crop-list.component.html',
 	styleUrl: './crop-list.component.scss',
 })
