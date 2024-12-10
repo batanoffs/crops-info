2edb68d62591bcd19aff52d194a8e4fec7335aa9
batanoffs <batanoff.s@protonmail.com>
2024-12-10 20:15:48 +0200

    update login component

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,27 +1,27 @@
-import { Component } from '@angular/core'
-import { Router } from '@angular/router'
-import { AuthService } from './auth.service'
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
 	selector: 'app-login',
 	standalone: true,
-	imports: [],
+	imports: [RouterLink],
 	templateUrl: './login.component.html',
 	styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-	username = ''
-	password = ''
-	loginFailed = false
+	username = '';
+	password = '';
+	loginFailed = false;
 
 	constructor(private authService: AuthService, private router: Router) {}
 
 	onLogin() {
-		const success = this.authService.login(this.username, this.password)
+		const success = this.authService.login(this.username, this.password);
 		if (success) {
-			this.router.navigate(['/home'])
+			this.router.navigate(['/home']);
 		} else {
-			this.loginFailed = true
+			this.loginFailed = true;
 		}
 	}
 }
