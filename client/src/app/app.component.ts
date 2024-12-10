73dafc5236e67975ecd938c8509b5a135cb098d9
batanoffs <batanoff.s@protonmail.com>
2024-12-10 17:25:11 +0200

    update imports

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navigation/navbar.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, NavbarComponent],
+	imports: [RouterOutlet, NavbarComponent, RouterLink, RouterLinkActive],
 	templateUrl: './app.component.html',
-	styleUrl: './app.component.scss',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 	title = 'crops-info';
