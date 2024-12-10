2d5e7b4e0c23dd332b750e9613487d45ba1e55ba
batanoffs <batanoff.s@protonmail.com>
2024-12-10 17:25:17 +0200

    add rooutes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,13 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
 import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
 	{ path: 'home', component: HomeComponent },
 	{ path: 'login', component: LoginComponent },
-	{ path: 'crops', component: CatalogComponent },
+	{ path: 'catalog', component: CatalogComponent },
+	{ path: 'catalog/:id', component: DetailsComponent },
 	// { path: 'register', component: RegisterComponent },
 	// { path: 'about', component: AboutComponent },
 	// { path: 'favorites', component: FavoritesComponent },
 	// { path: 'crop/:id', component: CropDetailComponent },
+	{ path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
