8958186db3e1c206c82c52a7d5ebe71368e23fbd
batanoffs <batanoff.s@protonmail.com>
2024-12-11 17:21:33 +0200

    add auth route

--- server/src/routes/mainRoutes.ts
+++ server/src/routes/mainRoutes.ts
@@ -1,9 +1,11 @@
 const express = require('express')
 
 import cropsRoutes from './crops'
+import authRoutes from './auth'
 
 const router = express.Router()
 
 router.use('/crops', cropsRoutes)
+router.use(authRoutes)
 
 export default router
