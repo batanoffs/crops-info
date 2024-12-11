b595f91549d64000a380caf85eda2cd1f4dd421e
batanoffs <batanoff.s@protonmail.com>
2024-12-11 23:27:32 +0200

    add button for authenticated user to add crop to favorites

--- client/src/app/components/details/details.component.ts
+++ client/src/app/components/details/details.component.ts
@@ -2,26 +2,47 @@ import { Component } from '@angular/core';
 import { Crop } from '../catalog/crop.interface';
 import { ActivatedRoute } from '@angular/router';
 import { CropService } from '../catalog/crop.service';
+import { AuthService } from '../../services/auth.service';
+import { NgIf } from '@angular/common';
+import { getToken } from '../../utils/cookie';
 
 @Component({
 	selector: 'app-details',
 	standalone: true,
-	imports: [],
+	imports: [NgIf],
 	templateUrl: './details.component.html',
 	styleUrl: './details.component.scss',
-	providers: [CropService],
+	providers: [CropService, AuthService],
 })
 export class DetailsComponent {
 	crop = {} as Crop;
-
-	constructor(private route: ActivatedRoute, private cropService: CropService) {}
+	isAuth = true;
+	constructor(
+		private route: ActivatedRoute,
+		private authService: AuthService,
+		private cropService: CropService
+	) {}
 
 	ngOnInit(): void {
-		// const id = this.route.snapshot.params;
+		const token = getToken();
+		if (token === '' || token === undefined || token === null) this.isAuth = false;
+		console.log(this.isAuth);
+
 		const id = this.route.snapshot.params['id'];
 
 		this.cropService.getOneCrop(id).subscribe((response: any) => {
 			this.crop = response;
+		});
+	}
+
+	addToFavorites(): void {
+		if (!this.isAuth) {
+			alert('Please log in to add this crop to favorites.');
+			return;
+		}
+
+		this.cropService.saveCrop(this.crop._id).subscribe(response => {
+			console.log('Crop added to favorites:', response);
 		});
 	}
 }
