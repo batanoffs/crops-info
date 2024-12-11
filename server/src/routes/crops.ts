9a0723ab147f75577fe8f1284944519e09c348cb
batanoffs <batanoff.s@protonmail.com>
2024-12-11 17:21:19 +0200

    add route to handle image uploads

--- server/src/routes/crops.ts
+++ server/src/routes/crops.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
-
+import { uploadCoverImage } from '../controllers/images'
 import { getAll, addCrop, getOne } from '../controllers/crops'
+import upload from '../middlewares/multer'
 
 const router = Router()
 
 router.get('/', getAll)
 router.post('/:id', addCrop)
 router.get('/:id', getOne)
+router.post('/upload', upload.single('picture'), uploadCoverImage)
 
 export default router
