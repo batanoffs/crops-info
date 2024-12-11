a3bac75cca511b6f22263d5262423bc6f022c9b0
batanoffs <batanoff.s@protonmail.com>
2024-12-11 23:05:23 +0200

    update endpoints

--- client/src/app/common/serverApi.ts
+++ client/src/app/common/serverApi.ts
@@ -1,13 +1,15 @@
-const baseURL = 'http://localhost:5000/api/'
+const baseURL = 'http://localhost:5000/api/';
 
 const endpoints = {
 	crops: baseURL + 'crops/',
 	auth: baseURL + 'auth/',
-}
+	favorites: baseURL + 'crops/favorites',
+};
 
 export const API = {
 	CATALOG: endpoints.crops,
 	LOGIN: endpoints.auth + 'login',
 	REGISTER: endpoints.auth + 'register',
 	LOGOUT: endpoints.auth + 'logout',
-}
+	FAVORITES: endpoints.favorites,
+};
