419d2e859c2a1ae7c0a845f88e284045ab423ffa
batanoffs <batanoff.s@protonmail.com>
2024-12-11 23:26:20 +0200

    add route for favorites

--- server/src/routes/crops.ts
+++ server/src/routes/crops.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { uploadCoverImage } from '../controllers/images'
 import { getAll, addCrop, getOne } from '../controllers/crops'
+import { addToFavorites } from '../controllers/favorites'
 import upload from '../middlewares/multer'
 
 const router = Router()
@@ -9,5 +10,6 @@ router.get('/', getAll)
 router.post('/:id', addCrop)
 router.get('/:id', getOne)
 router.post('/upload', upload.single('picture'), uploadCoverImage)
+router.post('/favorites', addToFavorites)
 
 export default router
