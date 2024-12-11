78d7f7039693cb5d2b0eeebe136cc7191263ed6c
batanoffs <batanoff.s@protonmail.com>
2024-12-11 17:20:52 +0200

    add user schema

---
+++ server/src/models/User.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose'
+import { IUserSchema } from '../interfaces/user.interface'
+
+const UserSchema: Schema = new Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		register_date: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{
+		collation: {
+			locale: 'en',
+			strength: 2,
+		},
+	}
+)
+
+const User = model<IUserSchema>('User', UserSchema)
+
+export default User
