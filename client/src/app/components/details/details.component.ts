cb5906459d3207072d05f5dbdf12dc6c6b960314
batanoffs <batanoff.s@protonmail.com>
2024-12-14 23:42:28 +0200

    update validation and errors, update overall html structure and css

--- client/src/app/components/details/details.component.ts
+++ client/src/app/components/details/details.component.ts
@@ -3,16 +3,17 @@ import { Crop } from '../catalog/crop.interface';
 import { ActivatedRoute } from '@angular/router';
 import { CropService } from '../catalog/crop.service';
 import { AuthService } from '../../services/auth.service';
-import { NgIf } from '@angular/common';
 import { getToken } from '../../utils/cookie';
+import { CommonModule } from '@angular/common';
+import { TransformAttr } from '../../directives/transformAttr.directive';
 
 @Component({
 	selector: 'app-details',
 	standalone: true,
-	imports: [NgIf],
+	imports: [CommonModule, TransformAttr],
 	templateUrl: './details.component.html',
 	styleUrl: './details.component.scss',
-	providers: [CropService, AuthService],
+	providers: [CropService, AuthService, ],
 })
 export class DetailsComponent {
 	crop = {} as Crop;
@@ -25,13 +26,17 @@ export class DetailsComponent {
 
 	ngOnInit(): void {
 		const token = getToken();
-		if (token === '' || token === undefined || token === null) this.isAuth = false;
+		if (!token) this.isAuth = false;
 		console.log(this.isAuth);
 
 		const id = this.route.snapshot.params['id'];
 
 		this.cropService.getOneCrop(id).subscribe((response: any) => {
-			this.crop = response;
+			if (this.hasAllAttributes(response)) {
+				this.crop = response;
+			} else {
+				console.error('Crop data is incomplete:', response);
+			}
 		});
 	}
 
@@ -44,5 +49,22 @@ export class DetailsComponent {
 		this.cropService.saveCrop(this.crop._id).subscribe(response => {
 			console.log('Crop added to favorites:', response);
 		});
+	}
+
+	private hasAllAttributes(crop: any): boolean {
+		const requiredAttributes = [
+			'_id',
+			'name',
+			'picture',
+			'description',
+			'attributes',
+			'nutrition',
+			'pests',
+			'diseases',
+			'companionPlants',
+			'combativePlants',
+			'createdAt',
+		];
+		return requiredAttributes.every(attr => crop.hasOwnProperty(attr));
 	}
 }
