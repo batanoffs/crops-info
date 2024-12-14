422a044d8d3601f9cecc38eedf175867c3b62df7
batanoffs <batanoff.s@protonmail.com>
2024-12-14 23:41:37 +0200

    add pagination logic in catalog and design

--- client/src/app/components/catalog/catalog.component.ts
+++ client/src/app/components/catalog/catalog.component.ts
@@ -13,12 +13,37 @@ import { CropService } from './crop.service';
 })
 export class CatalogComponent implements OnInit {
 	crops: Crop[] = [];
+	pageSize = 6;
+	page = 1;
 
 	constructor(private cropService: CropService) {}
 
 	ngOnInit(): void {
+		this.getCrops();
+	}
+
+	getCrops(): void {
 		this.cropService.getCrops().subscribe((response: any) => {
 			this.crops = response;
 		});
+	}
+
+	get pages(): number[] {
+		return Array.from(
+			{ length: Math.ceil(this.crops.length / this.pageSize) },
+			(_, i) => i + 1
+		);
+	}
+
+	get paginatedCrops(): Crop[] {
+		const start = (this.page - 1) * this.pageSize;
+		const end = this.page * this.pageSize;
+		return this.crops.slice(start, end);
+	}
+
+	goToPage(page: number): void {
+		if (page >= 1 && page <= this.pages.length) {
+			this.page = page;
+		}
 	}
 }
