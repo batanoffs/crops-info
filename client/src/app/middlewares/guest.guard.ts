f0eeab7a1db639c4f54173675621e71fb99d17d6
batanoffs <batanoff.s@protonmail.com>
2024-12-16 20:08:55 +0200

    add guards  for auth and guest users

---
+++ client/src/app/middlewares/guest.guard.ts
@@ -0,0 +1,39 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const guestGuard: CanActivateFn = (route, state) => {
+	const authService = inject(AuthService);
+	const router = inject(Router);
+	if (!authService.isAuthenticated) {
+		return true;
+	} else {
+		// Redirect to home page if already authenticated
+		router.navigate(['/home']);
+		return false;
+	}
+};
+
+// import { Injectable } from '@angular/core';
+// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+// import { Observable } from 'rxjs';
+// import { AuthService } from '../services/auth.service';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class GuestGuard implements CanActivate {
+//   constructor(private authService: AuthService, private router: Router) {}
+
+//   canActivate(
+//     next: ActivatedRouteSnapshot,
+//     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+//     if (!this.authService.isAuthenticated) {
+//       return true;
+//     } else {
+//       // Redirect to home page if already authenticated
+//       this.router.navigate(['/home']);
+//       return false;
+//     }
+//   }
+// }
