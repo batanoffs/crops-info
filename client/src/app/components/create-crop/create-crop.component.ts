642b2a7c7dd0a65c0d9a552bde5c5c48f6c4da33
batanoffs <batanoff.s@protonmail.com>
2024-12-16 20:07:45 +0200

    add new components

---
+++ client/src/app/components/create-crop/create-crop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-crop',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-crop.component.html',
+  styleUrl: './create-crop.component.scss'
+})
+export class CreateCropComponent {
+
+}
