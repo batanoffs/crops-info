eea2f6366bd3a07b9d8d3982c5b1249b4e1f82d4
batanoffs <batanoff.s@protonmail.com>
2024-12-16 20:07:27 +0200

    import guards and new routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,16 +5,19 @@ import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { RegisterComponent } from './components/register/register.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { CreateCropComponent } from './components/create-crop/create-crop.component';
+import { guestGuard } from './middlewares/guest.guard';
+import { authGuard } from './middlewares/auth.guard';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	{ path: 'home', component: HomeComponent },
-	{ path: 'login', component: LoginComponent },
+	{ path: 'login', component: LoginComponent, canActivate: [guestGuard] },
+	{ path: 'register', component: RegisterComponent, canActivate: [guestGuard] },
 	{ path: 'catalog', component: CatalogComponent },
 	{ path: 'catalog/:id', component: DetailsComponent },
-	{ path: 'register', component: RegisterComponent },
-	// { path: 'about', component: AboutComponent },
-	// { path: 'favorites', component: FavoritesComponent },
-	// { path: 'crop/:id', component: CropDetailComponent },
+	{ path: 'favorites', component: FavoritesComponent, canActivate: [authGuard] },
+	{ path: 'create', component: CreateCropComponent, canActivate: [authGuard] },
 	{ path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
