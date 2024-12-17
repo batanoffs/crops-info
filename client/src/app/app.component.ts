27ddf6e9225779138a3ca0b0feda1b2d832dd50d
batanoffs <batanoff.s@protonmail.com>
2024-12-18 01:05:02 +0200

    add provider

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navigation/navbar.component';
+import { AuthService } from './services/auth.service';
 
 @Component({
 	selector: 'app-root',
@@ -8,14 +9,11 @@ import { NavbarComponent } from './components/navigation/navbar.component';
 	imports: [RouterOutlet, NavbarComponent],
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss', '../styles.scss'],
+	providers: [AuthService],
 })
 export class AppComponent {
 	title = 'crops-info';
 	isNavbarMinimized = false;
-
-	constructor() {
-		console.log('isNavbarMinimized', this.isNavbarMinimized);
-	}
 
 	updateNavbarMinimized(value: boolean): void {
 		this.isNavbarMinimized = value;
