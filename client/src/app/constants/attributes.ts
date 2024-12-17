93aa500fdc811d56e8e969e0fb416041bbbed98b
batanoffs <batanoff.s@protonmail.com>
2024-12-18 01:09:00 +0200

    add constants for enum values

---
+++ client/src/app/constants/attributes.ts
@@ -0,0 +1,28 @@
+const sunOptions = ['Full Sun', 'Partial Sun', 'Shade'];
+const frostOptions = ['Not tolerant', 'Tolerant', 'Very tolerant'];
+const soilOptions = ['Acidic', 'Neutral', 'Alkaline'];
+const sowingTimeOptions = [
+	'Very Early Spring',
+	'Early Spring',
+	'Spring',
+	'Late Spring',
+	'Very Early Summer',
+	'Early Summer',
+	'Summer',
+	'Late Summer',
+	'Very Early Autumn',
+	'Early Autumn',
+	'Autumn',
+	'Late Autumn',
+	'Very Early Winter',
+	'Early Winter',
+	'Winter',
+	'Late Winter',
+];
+
+export const attributes = {
+	sunOptions,
+	frostOptions,
+	soilOptions,
+	sowingTimeOptions,
+};
