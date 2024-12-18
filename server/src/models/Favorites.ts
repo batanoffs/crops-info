2dd47c79a575be21f1a52d8f51c9fd5a7855d9e5
batanoffs <batanoff.s@protonmail.com>
2024-12-18 19:32:14 +0200

    update model

--- server/src/models/Favorites.ts
+++ server/src/models/Favorites.ts
@@ -1,20 +1,28 @@
-import { model, Schema, Document } from 'mongoose'
+import { model, Schema } from 'mongoose'
 import { IFavoritesSchema } from '../interfaces/Favorites.interface'
 
-const FavoritesSchema: Schema = new Schema({
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-	crops: [
-		{
+const FavoritesSchema: Schema = new Schema(
+	{
+		user: {
 			type: Schema.Types.ObjectId,
-			ref: 'Crop',
+			ref: 'User',
 			required: true,
+			unique: true,
 		},
-	],
-})
+		crops: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Crop',
+				required: true,
+			},
+		],
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+		_id: false,
+	}
+)
 
 const Favorites = model<IFavoritesSchema>('Favorites', FavoritesSchema)
 
