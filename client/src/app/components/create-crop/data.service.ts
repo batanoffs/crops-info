bbd8479cc438c63038b11d384e583893ac287600
batanoffs <batanoff.s@protonmail.com>
2024-12-18 23:45:54 +0200

    renamed

--- client/src/app/components/create-crop/data.service.ts
+++ client/src/app/components/create-crop/data.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
 import { API } from '../../common/serverApi';
 import { Observable } from 'rxjs';
+import { getToken } from '../../utils/cookie';
 
 @Injectable({
 	providedIn: 'root',
 })
-export class UploadService {
+export class DataService {
 	constructor(private http: HttpClient) {}
 
 	uploadImage(file: File): Observable<HttpEvent<any>> {
@@ -18,5 +19,18 @@ export class UploadService {
 		});
 
 		return this.http.request(req);
+	}
+
+	createCrop(data: any): Observable<any> {
+		const userToken = getToken();
+		if (!userToken) {
+			console.error('User not authorized');
+			return new Observable(observer => observer.error('User not authorized'));
+		}
+		return this.http.post(API.CREATE, data, {
+			headers: {
+				Authorization: `Bearer ${userToken}`,
+			},
+		});
 	}
 }
