22a44dafcdd953afe19b19a678444c422aa9becc
batanoffs <batanoff.s@protonmail.com>
2024-12-18 19:30:28 +0200

    update controller methods to respond properly

--- server/src/controllers/favorites.ts
+++ server/src/controllers/favorites.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from 'express'
 import Crop from '../models/Crop'
 import Favorites from '../models/Favorites'
 import { verifyToken } from '../services/jwt'
+import mongoose from 'mongoose'
 
 export const addToFavorites = async (req: Request, res: Response): Promise<void> => {
-	const { id } = req.params
+	const { id } = req.body
 
 	try {
 		//TODO validate and get userID from provided token
@@ -17,17 +18,32 @@ export const addToFavorites = async (req: Request, res: Response): Promise<void>
 
 		const decodedToken = verifyToken(token)
 		const userId = decodedToken._id
-		
+
 		const crop = await Crop.findById(id)
 		if (!crop) {
 			res.status(404).json({ message: 'Crop not found' })
 			return
 		}
 
-		const favorites = await Favorites.create({ user: userId, crops: [id] })
-		await favorites.save()
-		res.json(favorites)
+		const favorites = await Favorites.findOne({ user: userId })
+
+		if (!favorites) {
+			const newFavorites = await Favorites.create({ user: userId, crops: [id] })
+			await newFavorites.save()
+			res.json(newFavorites)
+		} else {
+			if (favorites.crops.includes(id)) {
+				res.status(400).json({ message: 'Crop already added to favorites' })
+				return
+			}
+
+			favorites.crops.push(id)
+			await favorites.save()
+			res.json(favorites)
+		}
 	} catch (error: unknown) {
+		console.error(error)
+
 		if (error instanceof Error) {
 			res.status(500).json({
 				message: 'Error adding crop to favorites',
@@ -36,6 +52,50 @@ export const addToFavorites = async (req: Request, res: Response): Promise<void>
 		} else {
 			res.status(500).json({
 				message: 'Unknown error adding crop to favorites',
+				error: 'Unknown error',
+			})
+		}
+	}
+}
+
+export const getFavorites = async (req: Request, res: Response): Promise<void> => {
+	try {
+		const token = req.headers.authorization?.split(' ')[1]
+
+		if (!token) {
+			res.status(401).json({ message: 'No token provided' })
+			return
+		}
+
+		const decodedToken = verifyToken(token)
+		const userId = decodedToken._id
+
+		if (!mongoose.Types.ObjectId.isValid(userId)) {
+			res.status(400).json({ message: 'Invalid Object ID' })
+			return
+		}
+
+		// Query Favorites
+		const favorites = await Favorites.findOne({ user: userId })
+			.populate({ path: 'crops', select: 'name picture' })
+			.exec()
+
+		if (!favorites) {
+			res.status(204).json({ message: 'No content' })
+			return
+		}
+
+		res.json(favorites)
+	} catch (error: unknown) {
+		console.error(error)
+		if (error instanceof Error) {
+			res.status(500).json({
+				message: 'Error fetching favorites',
+				error: error.message,
+			})
+		} else {
+			res.status(501).json({
+				message: 'Unknown error fetching favorites',
 				error: 'Unknown error',
 			})
 		}
